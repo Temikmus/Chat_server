b5738d366852d08b7e17251a3f5b576dd2cd9191
Артемий <104589663+Temikmus@users.noreply.github.com>
2024-05-09 21:01:32 +0300

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -9,4 +9,10 @@ int Client::id_count=0;
 Client::Client() {
     id=id_count++;
     name="noname";
+    email="email";
+    password="password";
+}
+
+void Client::print() const {
+    std::cout<<"Socket:"<<socket<<"  Email:"<<email<<"  Password:"<<password<<"  Name:"<<name<<std::endl;
 }
