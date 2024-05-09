ad5148fde8c0dd144e24aa55a77ea8cf67d21125
Артемий <104589663+Temikmus@users.noreply.github.com>
2024-05-09 21:01:52 +0300

    Update Client.h

--- Client.h
+++ Client.h
@@ -7,8 +7,11 @@
 class Client{
 public:
     Client();
+    void print() const;
     int id;
     std::string name;
+    std::string email;
+    std::string password;
     SOCKET socket;
 private:
     static int id_count;
